67f6b4144b66f29841249364161d3978d306a590
hotsaan <201677553+hotsaan@users.noreply.github.com>
2026-01-17 20:23:12 +0900

    추가 - RefreshResponse

---
+++ app/src/main/java/kr/ac/uc/albago/api/RefreshResponse.java
@@ -0,0 +1,10 @@
+package kr.ac.uc.albago.api;
+
+public class RefreshResponse {
+
+    public String accessToken;
+    public String refreshToken;
+
+    // Gson용 기본 생성자
+    public RefreshResponse() {}
+}
